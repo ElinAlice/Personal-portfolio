0f1559b71ea382c01f3d58f4dcda3338ae84f558
ElinAlice <nelializ02@gmail.com>
2020-08-24 15:17:42 -0500

    Feat: adding effects and transitions in styles

--- src/script/app.js
+++ src/script/app.js
@@ -10,58 +10,12 @@ menu.addEventListener("click", () => {
     }
 });
 
-const menuFocus = () => {
-    const target = document.querySelector(".target");
-    const links = document.querySelector("#displayNavMenu a");
-    const colors = ["deepskyblue", "orange", "firebrick", "gold", "magenta", "black", "darkblue"];
-
-    const mouseEnter = () => {
-        if(!this.parentNode.classList.contains("active"))
-        {
-            for(let i = 0; i < links.length; i++)
-            {
-                if(links[i].parentNode.classList.contains("active"))
-                {
-                    links[i].parentNode.classList.remove("active");
-                }
-                links[i].style.opacity = "0.25";
-            }
-            this.parentNode.classList.add("active");
-            this.style.opacity = "1";
-
-            const width = this.getBoundingClientRect().width;
-            const height = this.getBoundingClientRect().height;
-            const left = this.getBoundingClientRect().left + window.pageXOffset;
-            const top = this.getBoundingClientRect().top + window.pageYOffset;
-            const color = colors[Math.floor(Math.random() * colors.length)];
-
-            target.style.width = `${width}px`;
-            target.style.height = `${height}px`;
-            target.style.left = `${left}px`;
-            target.style.top = `${top}px`;
-            target.style.borderColor = color;
-            target.style.transform = "none";
-        }
+window.addEventListener('scroll', () => {
+    let animacion = document.querySelector('.rotateFooter');
+    let posicionFooter = animacion.getBoundingClientRect().top;
+    console.log(posicionFooter);
+    let screenSize = window.innerHeight/3.5;
+    if((posicionFooter) < screenSize) {
+        animacion.style.animacion = 'mover 1s ease-out';
     }
-
-    for (let i = 0; i < links.length; i++) {
-        links[i].addEventListener("click", (e) => e.preventDefault());
-        links[i].addEventListener("mouseenter", mouseenterFunc);
-      }
-    
-      function resizeFunc() {
-        const active = document.querySelector("#displayNavMenu li.active");
-    
-        if (active) {
-          const left = active.getBoundingClientRect().left + window.pageXOffset;
-          const top = active.getBoundingClientRect().top + window.pageYOffset;
-    
-          target.style.left = `${left}px`;
-          target.style.top = `${top}px`;
-        }
-      }
-    
-      window.addEventListener("resize", resizeFunc);
-}
-
-menuFocus();
+})
